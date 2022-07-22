64af9f2ca5dc7e4977901116fa401e04ea3d12cf
DevXian <coolman555@me.com>
2022-07-23 02:04:55 +0900

    feat(home): create appBar, data list, search

--- src/components/AppToolbar.tsx
+++ src/components/AppToolbar.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, InputBase, Toolbar, Typography, Button } from '@mui/material';
+import { Search as SearchIcon } from '@mui/icons-material';
+import { styled, alpha } from '@mui/material/styles';
+
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	'&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: 'inherit',
+	padding: theme.spacing(1, 1, 1, 0),
+	paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+	transition: theme.transitions.create('width'),
+	width: '100%',
+}));
 
 const AppToolbar: React.FC = (): JSX.Element => {
 	return (
-		<AppBar>
+		<AppBar position="static">
 			<Toolbar variant="dense" sx={{ justifyContent: 'space-between' }}>
 				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 					Find Issue
 				</Typography>
+				<Search>
+					<SearchIconWrapper>
+						<SearchIcon />
+					</SearchIconWrapper>
+					<StyledInputBase placeholder="검색..." inputProps={{ 'aria-label': 'search', width: '100%' }} />
+				</Search>
 				<Button color="inherit">홈</Button>
 				<Button color="inherit">설정</Button>
 			</Toolbar>
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { useDemoData } from '@mui/x-data-grid-generator';
+
 
 const Home: React.FC = (): JSX.Element => {
-	return <div style={{ backgroundColor: 'red', width: '100%', height: '100vh' }}> test </div>;
+	const { data } = useDemoData({
+		dataSet: 'Commodity',
+		rowLength: 100,
+		maxColumns: 6,
+	});
+
+	return (
+		<div style={{ height: 'calc(100vh - 50px)', width: '100%' }}>
+			<DataGrid
+				{...data}
+				components={{
+					Toolbar: GridToolbar,
+				}}
+			/>
+		</div>
+	);
 };
 
 export default Home;
