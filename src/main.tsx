fd1e2e1fbafd357c188d5f239b2a6deaa47d1633
DevXian <coolman555@me.com>
2022-07-23 00:35:50 +0900

    build(electron): build issue
    cannot use browser history at electron. change hashrouter.
    
    BREAKING CHANGE: router bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 // Routes
 import Home from '@routes/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 );
 
